2de6745670877bde08750acaa44ed68091e54029
adamrouss <aslan.tesaev@yandex.ru>
2022-12-25 00:28:49 +0300

    Counter

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
-}
-
+    
+    //MARK plusButton
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        count = count + 1
+        label.text = "\(count)"
+        if count == 1 {
+            minusButton.isHidden = false
+        }
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+        }
+    
+    //MARK minusButton
+    
+    @IBAction func clearButton(_ sender: Any) {
+        count = count - 1
+        label.text = "\(count)"
+        
+        if count <= 0 {
+            minusButton.isHidden = true
+            let generator = UINotificationFeedbackGenerator()
+                      generator.notificationOccurred(.error)
+        }
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+    }
+    }
